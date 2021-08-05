ae3994aa519b2757e7a69c26de8db2de726dda90
Yvan Duhamel <yvandev@outlook.fr>
2021-08-05 11:33:58 +0200

    fix syntax highlighting

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -3,6 +3,7 @@ import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
 import json from 'highlight.js/lib/languages/json';
 import bash from 'highlight.js/lib/languages/bash';
+import xml from 'highlight.js/lib/languages/xml';
 import ini from 'highlight.js/lib/languages/ini';
 import yaml from 'highlight.js/lib/languages/yaml';
 import markdown from 'highlight.js/lib/languages/markdown';
@@ -11,6 +12,7 @@ import powershell from 'highlight.js/lib/languages/powershell';
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('json', json);
 hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('html', xml);
 hljs.registerLanguage('ini', ini);
 hljs.registerLanguage('toml', ini);
 hljs.registerLanguage('yaml', yaml);
@@ -18,7 +20,7 @@ hljs.registerLanguage('md', markdown);
 hljs.registerLanguage('powershell', powershell);
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('pre code').forEach((block) => {
-    hljs.highlightBlock(block);
+  document.querySelectorAll('pre code:not(.language-mermaid)').forEach((block) => {
+    hljs.highlightElement(block);
   });
 });
